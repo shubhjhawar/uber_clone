44211598e1715b83e335df9ab3e3c760b6583675
Shubh <shubhjhawar78@gmail.com>
2023-10-10 18:32:10 -0400

    keyboard avoid view implemented

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
@@ -15,34 +15,31 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen 
-              name="HomeScreen"
-              component={HomeScreen}
-              options={{
-                headerShown:false
-              }}
-            />
+          <KeyboardAvoidingView 
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={{flex:1}}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen 
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown:false
+                }}
+              />
 
-            <Stack.Screen 
-              name="MapScreen"
-              component={MapScreen}
-              options={{
-                headerShown:false
-              }}
-            />
-          </Stack.Navigator>
+              <Stack.Screen 
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown:false
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -11,11 +11,11 @@ const MapScreen = () => {
   return (
     <View>
 
-      <View style={tw`h-1/3`}>
+      <View style={tw`h-1/2`}>
         <Map />
       </View>
 
-      <View style={tw`h-2/3`}>
+      <View style={tw`h-1/2`}>
         <Stack.Navigator>
             <Stack.Screen
                 name="NavigateCard"
