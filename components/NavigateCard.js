304e1c8f7335027f385059bb0d8751f06e4ee4fd
Shubh <shubhjhawar78@gmail.com>
2023-10-10 14:22:40 -0400

    added few routes

---
+++ components/NavigateCard.js
@@ -0,0 +1,66 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { useDispatch } from 'react-redux';
+import { setDestination } from '../slices/navSlice';
+import { useNavigation } from '@react-navigation/core';
+
+const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView style={tw`flex-1 bg-white`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Morning, User</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
+        <View>
+          <GooglePlacesAutocomplete 
+            placeholder='Where to?'
+            debounce={400}
+            styles={toInputBoxStyles}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language:'en',
+            }}
+            fetchDetails={true}
+            returnKeyType={'search'}
+            minLength={2}
+            onPress={(data, details=null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              )
+              // console.log(data);
+              // console.log(details);
+
+              navigation.navigate('RideOptionsCard')
+            }}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default NavigateCard
+
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: '#DDDDDF',
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer:{
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  }
+})
